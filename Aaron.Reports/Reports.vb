04665b9f403acaebf1af863d20de418261ec48a3
Aaron Campf <aaroncampf@gmail.com>
2024-05-07 09:30:17 -0700

    Fixed AsPDF()
    Look, its a bit of a workaround but its what I figured out

--- Aaron.Reports/Reports.vb
+++ Aaron.Reports/Reports.vb
@@ -187,7 +187,7 @@ Public Class Basic
 	End Function
 
 
-	Private Shared Function AsStream(Document As XpsDocument, Page As Integer) As IO.MemoryStream
+	Private Shared Function AsStream(Document As XpsDocument, Page As Integer) As String
 		Dim bitmapEncoder As New Media.Imaging.JpegBitmapEncoder
 		Dim documentPage As DocumentPage = Document.GetFixedDocumentSequence.DocumentPaginator.GetPage(Page)
 		Dim targetBitmap As New Media.Imaging.RenderTargetBitmap(documentPage.Size.Width * 5, documentPage.Size.Height * 5, 96.0 * 5, 96.0 * 5, Media.PixelFormats.Pbgra32)
@@ -195,22 +195,25 @@ Public Class Basic
 		targetBitmap.Render(documentPage.Visual)
 		bitmapEncoder.Frames.Add(Media.Imaging.BitmapFrame.Create(targetBitmap))
 
-		Dim Stream As New IO.MemoryStream
-		bitmapEncoder.Save(Stream)
-		Return Stream
+		Dim TempFile As String = My.Computer.FileSystem.GetTempFileName
+		Using Stream As New IO.FileStream(TempFile, FileMode.Create)
+			bitmapEncoder.Save(Stream)
+		End Using
+
+		Return TempFile
 	End Function
 
 	''' <summary>
 	''' 
 	''' </summary>
 	''' <param name="Resize"></param>
-	''' <param name="Image"></param>
+	''' <param name="ImagePath"></param>
 	''' <param name="oPdfDoc"></param>
 	''' <param name="oPdfWriter"></param>
 	''' <remarks></remarks>
 	''' <stepthrough></stepthrough>
-	Shared Sub AddPage(Resize As Boolean, Image As IO.MemoryStream, ByRef oPdfDoc As iTextSharp.text.Document, oPdfWriter As iTextSharp.text.pdf.PdfWriter)
-		Dim oImage As iTextSharp.text.Image = iTextSharp.text.Image.GetInstance(Image)
+	Shared Sub AddPage(Resize As Boolean, ImagePath As String, ByRef oPdfDoc As iTextSharp.text.Document, oPdfWriter As iTextSharp.text.pdf.PdfWriter)
+		Dim oImage As iTextSharp.text.Image = iTextSharp.text.Image.GetInstance(ImagePath)
 		Dim iWidth As Single = oImage.Width, iHeight As Single = oImage.Height
 
 		If Resize Then
